bbc1c3b76cca5e6b680068de6a5d8cd25104f5e7
venkatesh <venkikumar1110@gmail.com>
2025-12-22 17:02:50 +0530

    implement allure report

---
+++ test_types/test_Sanity.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import pytest
+import time
+from Pages.Launch_website_application import Launch_website_application
+from credentails import credentials
+from Pages.login import Login as login
+from Pages.Add_employee_admin import Add_employee_admin
+global launch_browser
+
+def test_start_the_application (launch_browser):
+    launch = Launch_website_application(launch_browser)
+    launch.load("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    launch_browser.save_screenshot("D:/Python_Automation/screenshots/launch_browser.png")
+    print("Website launched successfully")
+ #----------------------------LOGIN --------------------------#  
+def test_login_to_application(launch_browser):
+    login_page = login(launch_browser)
+    login_page.load("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    login_page.login_to_application(credentials.username, credentials.password)
+    launch_browser.save_screenshot("D:/Python_Automation/screenshots/login_successful.png")
+    print("Login to application successful")
+    assert "OrangeHRM" in launch_browser.title
+    time.sleep(2)
+   
+
